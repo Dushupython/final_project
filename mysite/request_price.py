c67235ba37a25de417fa09e930d4d703765418a4
Your Name <you@example.com>
2020-03-07 20:38:21 +0300

    2

--- mysite/request_price.py
+++ mysite/request_price.py
@@ -6,6 +6,3 @@ BASE_URL = f'https://api.binance.com/api/v3/ticker/price?symbol='
 def symbol(sym: str):
     return BASE_URL + sym
 
-
-response = requests.get(symbol('BTCUSDT'))
-print(response.text)
