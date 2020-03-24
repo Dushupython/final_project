2c4198c9848e86f18beaa0861da279581dab9934
Agri1988 <2690736@gmail.com>
2020-03-24 15:23:36 +0300

    Update signals.py

--- mysite/signals.py
+++ mysite/signals.py
@@ -5,5 +5,7 @@ from mysite.compare import comparison
 def Alert(sender: Bitcoin, instance: Bitcoin, **kwargs):
     # trigger_price = float(sender.objects.values('trigger_price'))
     # comparison(trigger_price)
-    last_element = sender.objects.get('trigger_price')
+    # last_element = sender.objects.get('trigger_price')
+    trigger_price = instance.trigger_price
+    comparison(trigger_price)
     print(last_element)
